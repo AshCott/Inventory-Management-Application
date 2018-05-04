dd553821a56726be7bca6d2935d385af46f98331
ACottrell <Ashley.cottrell@outlook.com>
2018-05-04 14:31:36 +1000

    Wrote test for getting store information
    Specifically getting the store capital and getting the store name

--- src/testPrograms/ItemClassTests.java
+++ src/testPrograms/ItemClassTests.java
@@ -6,14 +6,18 @@ import java.util.List;
 
 import org.junit.*;
 
-import program.Item;
 import program.IOCSV;
 
 public class ItemClassTests {
+	IOCSV importer;
+	
+	@Before
+	public void constructor() {
+		importer = new IOCSV();
+	}
 
 	@Test
 	public void ReadItem_PropertiesFile() throws IOException {
-		IOCSV importer = new IOCSV();
 		ArrayList<List> b = importer.readCSVFile("item_properties.csv");
 		System.out.println(b);
 	}
--- src/testPrograms/StoreClassTests.java
+++ src/testPrograms/StoreClassTests.java
@@ -2,17 +2,28 @@ package testPrograms;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import org.junit.*;
 
 import program.Store;
 
 public class StoreClassTests {
-
+	Store a;
+	
+	@Before
+	public void constructor() {
+		a = new Store();
+		a.setCapital(1000000.00);
+		a.setName("Woolworths");
+	}
+	
 	@Test
 	public void GetStoreCapital() {
-		Store a = new Store();
-		System.out.println(a.getStoreCapital());
 		assertEquals("$1,000,000.00", a.getStoreCapital());
+	}
+	
+	@Test
+	public void GetStoreName() {
+		assertEquals("Woolworths", a.getStoreName());
 	}
 
 }
