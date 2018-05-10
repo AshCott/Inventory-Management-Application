764d14ba0dcfc1545152849164bcf1456f0b563a
ACottrell <Ashley.cottrell@outlook.com>
2018-05-10 19:36:45 +1000

    Fixed up some tests that had to be changed due to complications

--- src/testPrograms/ItemClassTests.java
+++ src/testPrograms/ItemClassTests.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.*;
-
-import program.IOCSV;
+import program.*;
 
 public class ItemClassTests {
 	IOCSV importer;
@@ -18,8 +17,26 @@ public class ItemClassTests {
 
 	@Test
 	public void ReadItem_PropertiesFile() throws IOException {
-		ArrayList<List> b = importer.readCSVFile("item_properties.csv");
-		System.out.println(b);
+		//ArrayList<List> b = importer.readCSVFile("item_properties.csv");
+		//System.out.println(b);
+	}
+	
+	@Test
+	public void importItemProperties() throws IOException {
+		Store a = new Store();
+		a.creatInventory("item_properties.csv");
+	}
+	
+	@Test
+	public void importManifest() throws IOException {
+		Store a = new Store();
+		a.importManifest("manifest.csv");
+	}
+	
+	@Test
+	public void importSalesLog() throws IOException {
+		Store a = new Store();
+		a.importSalesLog("sales_log_0.csv");
 	}
 	
 }
