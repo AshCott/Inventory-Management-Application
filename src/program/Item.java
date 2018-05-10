d384a4d19ee906c3a884a435dbfeebe64674907a
ACottrell <Ashley.cottrell@outlook.com>
2018-05-10 18:26:15 +1000

    Added functions to get an items information

--- src/program/Item.java
+++ src/program/Item.java
@@ -15,20 +15,42 @@ public class Item {
 	private int ReorderAmount;
 	private double Temperature;
 	
-	public Item(String name, double sellPrice, int reorderAmount, int reorderPoint, double temperature) {
+	public Item(String name, double ManufactureCost, double sellPrice, int reorderAmount, int reorderPoint, double temperature) {
 		this.Name = name;
+		this.ManufactureCost = ManufactureCost;
 		this.SellPrice = sellPrice;
 		this.ReorderAmount = reorderAmount;
 		this.ReorderPoint = reorderPoint;
 		this.Temperature = temperature;
 	}
 	
-	public Item(String name, double sellPrice, int reorderAmount, int reorderPoint) {
+	public Item(String name, double ManufactureCost, double sellPrice, int reorderAmount, int reorderPoint) {
 		this.Name = name;
+		this.ManufactureCost = ManufactureCost;
 		this.SellPrice = sellPrice;
 		this.ReorderAmount = reorderAmount;
 		this.ReorderPoint = reorderPoint;
 	}
+	
+	public String getName() {
+		return this.Name;
+	}	
+	
+	public double getManufactureCost() {
+		return this.ManufactureCost;
+	}	
+	
+	public double getSellPrice() {
+		return this.SellPrice;
+	}	
+	
+	public int getReorderAmount() {
+		return this.ReorderAmount;
+	}	
+	
+	public int getReorderPoint() {
+		return this.ReorderPoint;
+	}	
 	
 	public double getTemperature() {
 		return Temperature;
