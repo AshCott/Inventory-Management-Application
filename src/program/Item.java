644fa69b51b71b8d9c76fa859837315e1d322c63
radhiDj <30493713+radhiDj@users.noreply.github.com>
2018-05-11 00:38:21 +1000

    make get and set

--- src/program/Item.java
+++ src/program/Item.java
@@ -16,22 +16,64 @@ public class Item {
 	private double Temperature;
 	
 	public Item(String name, double sellPrice, int reorderAmount, int reorderPoint, double temperature) {
-		this.Name = name;
-		this.SellPrice = sellPrice;
-		this.ReorderAmount = reorderAmount;
-		this.ReorderPoint = reorderPoint;
+		this.setName(name);
+		this.setSellPrice(sellPrice);
+		this.setReorderAmount(reorderAmount);
+		this.setReorderPoint(reorderPoint);
 		this.Temperature = temperature;
 	}
 	
 	public Item(String name, double sellPrice, int reorderAmount, int reorderPoint) {
-		this.Name = name;
-		this.SellPrice = sellPrice;
-		this.ReorderAmount = reorderAmount;
-		this.ReorderPoint = reorderPoint;
+		this.setName(name);
+		this.setSellPrice(sellPrice);
+		this.setReorderAmount(reorderAmount);
+		this.setReorderPoint(reorderPoint);
 	}
 	
+	//make get and set
 	public double getTemperature() {
 		return Temperature;
-	}	
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public double getManufactureCost() {
+		return ManufactureCost;
+	}
+
+	public void setManufactureCost(double manufactureCost) {
+		ManufactureCost = manufactureCost;
+	}
+
+	public int getReorderPoint() {
+		return ReorderPoint;
+	}
+
+	public void setReorderPoint(int reorderPoint) {
+		ReorderPoint = reorderPoint;
+	}
+
+	public int getReorderAmount() {
+		return ReorderAmount;
+	}
+
+	public void setReorderAmount(int reorderAmount) {
+		ReorderAmount = reorderAmount;
+	}
+
+	public double getSellPrice() {
+		return SellPrice;
+	}
+
+	public void setSellPrice(double sellPrice) {
+		SellPrice = sellPrice;
+	}
+	
 
 }
