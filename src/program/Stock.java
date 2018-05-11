86dbc83dd53dad954bc4bc542ddccf01c11723be
ACottrell <Ashley.cottrell@outlook.com>
2018-05-11 15:04:59 +1000

    Updated stock class with comments and some minor test methods

--- src/program/Stock.java
+++ src/program/Stock.java
@@ -11,27 +11,32 @@ import java.util.List;
  */
 public class Stock {
 	public static HashMap<String, Item> inventory = new HashMap<String, Item>();
+	public HashMap<String, Integer> itemList = new HashMap<String, Integer>();
 	
 	public Stock() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	public void creatInventory(ArrayList<List> inventorylist) {
-		for(List i : inventorylist) {
-			String name =  (String) i.get(0);
-			double ManufactureCost = Double.parseDouble((String) i.get(1));
-			double SellPrice = Double.parseDouble((String) i.get(2));
-			int ReorderPoint = Integer.parseInt((String) i.get(3));
-			int ReorderAmount = Integer.parseInt((String) i.get(4)); 
-			if(i.size() == 6) {
-				int Tempreture = Integer.parseInt((String) i.get(5));
-				//System.out.println(name+", "+ManufactureCost+", "+SellPrice+", "+ReorderPoint+", "+ReorderAmount+", "+Tempreture);
-				Item a = new Item(name,ManufactureCost,SellPrice,ReorderPoint,ReorderAmount,Tempreture);
-				inventory.put(name, a);
+		for(List item : inventorylist) {
+			
+			//Extract Item infomation from array
+			String name =  (String) item.get(0);
+			double ManufactureCost = Double.parseDouble((String) item.get(1));
+			double SellPrice = Double.parseDouble((String) item.get(2));
+			int ReorderPoint = Integer.parseInt((String) item.get(3));
+			int ReorderAmount = Integer.parseInt((String) item.get(4)); 
+			
+			//Check if tempreture is required and creat an item.
+			if(item.size() == 6) {
+				int Tempreture = Integer.parseInt((String) item.get(5));
+				//Create item with temprature
+				Item product = new Item(name,ManufactureCost,SellPrice,ReorderPoint,ReorderAmount,Tempreture);
+				inventory.put(name, product);
 			}else {
-				//System.out.println(name+", "+ManufactureCost+", "+SellPrice+", "+ReorderPoint+", "+ReorderAmount);
-				Item a = new Item(name,ManufactureCost,SellPrice,ReorderPoint,ReorderAmount);
-				inventory.put(name, a);
+				//Create item without temprature
+				Item product = new Item(name,ManufactureCost,SellPrice,ReorderPoint,ReorderAmount);
+				inventory.put(name, product);
 			}
 			
 		}
@@ -39,5 +44,10 @@ public class Stock {
 	
 	public Item getItem(String name){
 		return inventory.get(name);
+	}
+	
+	public void increaseItemAmount(String name) {
+		int a = inventory.get(name).getCurrentInventory();
+		inventory.get(name).setCurrentInventory(5557);
 	}
 }
