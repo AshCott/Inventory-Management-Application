62116407f798ed67d35b0607f0a337cf48aa121c
ACottrell <Ashley.cottrell@outlook.com>
2018-05-11 15:04:34 +1000

    Added comments for methods

--- src/program/Item.java
+++ src/program/Item.java
@@ -14,7 +14,17 @@ public class Item {
 	private int ReorderPoint;
 	private int ReorderAmount;
 	private double Temperature;
+	private int CurrentInventory;
 	
+	/**
+	 * Create a new Refreigerated Item
+	 * @param name
+	 * @param ManufactureCost
+	 * @param sellPrice
+	 * @param reorderAmount
+	 * @param reorderPoint
+	 * @param temperature
+	 */
 	public Item(String name, double ManufactureCost, double sellPrice, int reorderAmount, int reorderPoint, double temperature) {
 		this.Name = name;
 		this.ManufactureCost = ManufactureCost;
@@ -22,24 +32,46 @@ public class Item {
 		this.ReorderAmount = reorderAmount;
 		this.ReorderPoint = reorderPoint;
 		this.Temperature = temperature;
+		this.CurrentInventory = 0;
 	}
 	
+	/**
+	 * Create a new Unrefregerated Item 
+	 * @param name
+	 * @param ManufactureCost
+	 * @param sellPrice
+	 * @param reorderAmount
+	 * @param reorderPoint
+	 */
 	public Item(String name, double ManufactureCost, double sellPrice, int reorderAmount, int reorderPoint) {
 		this.Name = name;
 		this.ManufactureCost = ManufactureCost;
 		this.SellPrice = sellPrice;
 		this.ReorderAmount = reorderAmount;
 		this.ReorderPoint = reorderPoint;
+		this.CurrentInventory = 0;
 	}
 	
+	/**
+	 * 
+	 * @return The name of the item as a String
+	 */
 	public String getName() {
 		return this.Name;
 	}	
 	
+	/**
+	 * 
+	 * @return The manufacture cost of the Item as a Double
+	 */
 	public double getManufactureCost() {
 		return this.ManufactureCost;
 	}	
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public double getSellPrice() {
 		return this.SellPrice;
 	}	
@@ -55,5 +87,12 @@ public class Item {
 	public double getTemperature() {
 		return Temperature;
 	}	
-
+	
+	public int getCurrentInventory() {
+		return CurrentInventory;
+	}
+	
+	public void setCurrentInventory(int number) {
+		CurrentInventory = number;
+	}
 }
