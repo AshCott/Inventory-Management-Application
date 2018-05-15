37e184d8da9fdd426e218c7b6ed2049d758f08b6
radhiDj <30493713+radhiDj@users.noreply.github.com>
2018-05-15 18:49:22 +1000

    add new method for temperature and adding item

--- src/program/RefrigratedTruck.java
+++ src/program/RefrigratedTruck.java
@@ -4,14 +4,13 @@ public class RefrigratedTruck extends Truck {
 	double temperature;
 	
 	public RefrigratedTruck () {
-
+		super();
 	}
 	@Override
 	public double costCalculation(double temperature) {
 		this.temperature = temperature;
 		cost = 900+(200*Math.pow(0.7, (temperature/5)));
 		return cost;
-		
 	}
 	@Override
 	public double cargoCaps () {
@@ -19,8 +18,16 @@ public class RefrigratedTruck extends Truck {
 		return capacity;
 	}
 	
-	public void getTemperature() {
-		
+	public double getTemperature() {
+		return temperature;
 	}
-
+	@Override
+	public void addItem(String itemName, Integer quantity) {
+		items.put(itemName, quantity);
+		itemCount += quantity;
+ 	}
+	
+	public void setTemperature(double temperature) {
+		this.temperature = temperature;
+	}
 }
