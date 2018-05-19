47f11ba98746512d228c19e47c82a80d87f655b9
radhiDj <30493713+radhiDj@users.noreply.github.com>
2018-05-19 20:28:17 +1000

    change all of the abstract truck method to be abstract

--- src/program/Truck.java
+++ src/program/Truck.java
@@ -7,48 +7,13 @@ public abstract class Truck {
 	protected double capacity;
 	protected double cost;
 	protected Integer itemCount;
-	private String truckType;
 	protected HashMap<String, Integer> items;
-	private double totalPrice;
-	
-	public Truck() {
-		this.items = new HashMap<String, Integer>();
-		this.itemCount = 0; // assume the truck starts empty
-	}
-	
-	public Integer getItemCount() {
-		return this.itemCount;
-	}
-	
-	public double costCalculation(double tempQuant) {
-		return cost;
-	}
-	public double cargoCaps() {
-		return capacity;
-	}
-	
-	public String getTruckType() {
-		
-		return truckType;
-	}
-	public void setTruckType(String truckType) {
-		this.truckType = truckType;
-	}
-	
-	public void addItem(String itemName, Integer quantity) {
-		items.put(itemName, quantity);
-		itemCount += quantity;
-	}
-	public void getItem(String itemName) {
-		items.get(itemName);
-	}
-	public void setTotalPriceInTruck(double totalPrice) {
-		this.totalPrice = totalPrice;
-	}
-	public double getTotalPriceInTruck() {
-
-		return totalPrice;
-	}
-	
-
+	protected double totalPrice;
+	public abstract double costCalculation(double tempQuant);
+	public abstract void addItem(String itemName, Integer quantity);
+	public abstract boolean isCargoFull();
+	public abstract void setQuantity(double quantity);
+	public abstract double getQuantity();
+	public abstract void setTotalPriceInTruck(double totalPrice);
+	public abstract double getTotalPriceInTruck();
 }
