8054b8e804f94e3e39b15f6ae65f5e593191da96
ACottrell <Ashley.cottrell@outlook.com>
2018-05-24 17:23:49 +1000

    Updated Item Comments

--- src/program/Item.java
+++ src/program/Item.java
@@ -2,25 +2,27 @@ package program;
 
 /**
  * 
- * @author 
+ * @author Ashley Cottrell
  * @author
  *
  */
 public class Item {
-	
+
 	private String Name;
 	private double ManufactureCost;
 	private double SellPrice;
 	private int ReorderPoint;
 	private int ReorderAmount;
-	//make the temperature to be nan so that if its a dry goods it doesn't' have a temperature
+	// make the temperature to be nan so that if its a dry goods it doesn't' have a
+	// temperature
 	private double Temperature = Double.NaN;
 	private int CurrentInventory;
 	private boolean hasTemp;
 	private int quantity;
-	
+
 	/**
-	 * Create a new Refreigerated Item
+	 * Create a new Refrigerated Item
+	 * 
 	 * @param name
 	 * @param ManufactureCost
 	 * @param sellPrice
@@ -28,7 +30,8 @@ public class Item {
 	 * @param reorderPoint
 	 * @param temperature
 	 */
-	public Item(String name, double ManufactureCost, double sellPrice, int reorderAmount, int reorderPoint, double temperature) {
+	public Item(String name, double ManufactureCost, double sellPrice, int reorderAmount, int reorderPoint,
+			double temperature) {
 		this.Name = name;
 		this.ManufactureCost = ManufactureCost;
 		this.SellPrice = sellPrice;
@@ -38,9 +41,10 @@ public class Item {
 		this.hasTemp = true;
 		this.CurrentInventory = 0;
 	}
-	
+
 	/**
-	 * Create a new Unrefregerated Item 
+	 * Create a new Un-refrigerated Item
+	 * 
 	 * @param name
 	 * @param ManufactureCost
 	 * @param sellPrice
@@ -56,54 +60,90 @@ public class Item {
 		this.CurrentInventory = 0;
 		this.hasTemp = false;
 	}
-	
+
 	/**
+	 * Returns the Items Name
 	 * 
 	 * @return The name of the item as a String
 	 */
 	public String getName() {
 		return this.Name;
-	}	
-	
+	}
+
 	/**
+	 * Returns the items manufacture cost
 	 * 
 	 * @return The manufacture cost of the Item as a Double
 	 */
 	public double getManufactureCost() {
 		return this.ManufactureCost;
-	}	
-	
+	}
+
 	/**
+	 * Returns the sell price of an item
 	 * 
-	 * @return
+	 * @return The sell price as a double
 	 */
 	public double getSellPrice() {
 		return this.SellPrice;
-	}	
-	
+	}
+
+	/**
+	 * Get the re-order amount
+	 * 
+	 * @return the re-order amount as a integer
+	 */
 	public int getReorderAmount() {
 		return this.ReorderAmount;
-	}	
-	
+	}
+
+	/**
+	 * gets teh re-order point
+	 * 
+	 * @return the re-order point as a integer
+	 */
 	public int getReorderPoint() {
 		return this.ReorderPoint;
-	}	
-	
+	}
+
+	/**
+	 * Check if an item has a tempreture
+	 * 
+	 * @return boolean | true - Item has tempreture | False - Item doesnt have a
+	 *         tempreture
+	 */
 	public boolean hasTempreture() {
-		if(hasTemp) {
+		if (hasTemp) {
 			return true;
-		}else {
+		} else {
 			return false;
 		}
 	}
+
+	/**
+	 * get the tempreture of an item
+	 * 
+	 * @return the tempreture as a double
+	 */
 	public double getTemperature() {
 		return Temperature;
-	}	
-	
+	}
+
+	/**
+	 * Get the current inventory of an item
+	 * 
+	 * @return the current inventory as a integer
+	 */
 	public int getCurrentInventory() {
 		return CurrentInventory;
 	}
-	
+
+	/**
+	 * Set the current inventory of an item
+	 * 
+	 * @param number
+	 *            value that the current inventory will be changed to
+	 */
 	public void setCurrentInventory(int number) {
 		CurrentInventory = number;
 	}
